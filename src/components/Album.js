06c228dcbdb3f973cbcd498b17b2aa914b4a23a1
Pensor <patrick.feldtmann@gmail.com>
2020-06-23 13:56:01 +0200

    better image quality

--- src/components/Album.js
+++ src/components/Album.js
@@ -13,11 +13,13 @@ function Album({ release, setPlay, setPlayURIs }) {
 			}}
 		>
 			<div className='flex flex-col bg-white shadow-lg overflow-hidden'>
-				<img src={release.images[2].url} alt='' className='w-64 h-64 mx-auto' />
+				<img src={release.images[1].url} alt={release.name} className='w-64 h-64' />
 				<div className='px-6 py-4'>
 					<div className='flex items-center pt-3'>
 						<div className='ml-4'>
-							<p className='font-bold'>{release.name}</p>
+							<p className='font-bold'>
+								{release.name.length > 25 ? release.name.substring(0, 22) + '...' : release.name}
+							</p>
 							<p className='text-sm text-gray-700 mt-1'>{release.artists[0].name}</p>
 						</div>
 					</div>
