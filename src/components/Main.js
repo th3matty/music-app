8b1778a7b9d6047362ea39be2758032f9e642c59
th3matty <ma_malek@protonmail.com>
2020-06-23 14:33:15 +0200

    right frame for lyrics added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,65 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import SpotifyPlayer from 'react-spotify-web-playback'; // https://github.com/gilbarbara/react-spotify-web-playback
-import Aside from './Aside.js';
-import Album from './Album';
+import React, { useEffect, useState } from "react";
+import SpotifyPlayer from "react-spotify-web-playback"; // https://github.com/gilbarbara/react-spotify-web-playback
+import Aside from "./Aside.js";
+import Album from "./Album";
 
 function Main({ token }) {
-	const [releases, setReleases] = useState([]);
-	const [playURIs, setPlayURIs] = useState([]);
-	const [play, setPlay] = useState(null);
+  const [releases, setReleases] = useState([]);
+  const [playURIs, setPlayURIs] = useState([]);
+  const [play, setPlay] = useState(null);
 
-	useEffect(() => {
-		fetch('https://api.spotify.com/v1/browse/new-releases', {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		})
-			.then(res => res.json())
-			.then(res => {
-				console.log(res.albums.items);
-				setReleases(res.albums.items);
-			});
-	}, [token]);
+  useEffect(() => {
+    fetch("https://api.spotify.com/v1/browse/new-releases", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.albums.items);
+        setReleases(res.albums.items);
+      });
+  }, [token]);
 
-	return (
-		<div className='flex flex-col h-screen text-sm text-gray-400'>
-			<div className='flex-1 flex overflow-y-hidden'>
-				<div className='sidebar w-48 flex-none flex flex-col justify-between bg-gray_aside font-semibold'>
-					<div className='py-6'>
-						here comes our Logo, import it here "main.js"
-						<Aside />
-					</div>
-					<div className='py-6'></div>
-					<div className='overflow-y-auto bg-scroll container'>
-						<p>Playlists</p>
-					</div>
-				</div>
-				<div className='content-area flex-1 overflow-y-auto bg-gray_content'>
-					This is the content area
-					<h2>New Releases</h2>
-					<div>
-						{releases.map(release => (
-							<Album setPlay={setPlay} setPlayURIs={setPlayURIs} release={release} />
-						))}
-					</div>
-				</div>
-				Lyrics
-			</div>
-			<div>
-				<div className='bg-gray_lyrics'>
-					bottom
-					<SpotifyPlayer
-						token={token}
-						uris={playURIs}
-						play={play}
-						callback={data => {
-							setPlay(data.isPlaying);
-						}}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col h-screen text-sm text-gray-400">
+      <div className="flex-1 flex overflow-y-hidden">
+        <div className="sidebar w-48 flex-none flex flex-col justify-between bg-gray_aside font-semibold">
+          <div className="py-6">
+            here comes our Logo, import it here "main.js"
+            <Aside />
+          </div>
+          <div className="py-6"></div>
+          <div className="overflow-y-auto bg-scroll container">
+            <p>Playlists</p>
+          </div>
+        </div>
+        <div className="content-area flex-1 overflow-y-auto bg-gray_content h-screen">
+          This is the content area
+          <h2>New Releases</h2>
+          <div>
+            {releases.map((release) => (
+              <Album
+                setPlay={setPlay}
+                setPlayURIs={setPlayURIs}
+                release={release}
+              />
+            ))}
+          </div>
+        </div>
+        <div className=" w-56 bg-gray-600">
+          {" "}
+          <h2>rechtes frage</h2>
+          <br></br>
+          <h4>PlatzHalter für Osama</h4>
+          Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+          amet.
+        </div>
+      </div>
+      <div>
+        <div className="">
+          <SpotifyPlayer
+            token={token}
+            uris={playURIs}
+            play={play}
+            callback={(data) => {
+              setPlay(data.isPlaying);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
