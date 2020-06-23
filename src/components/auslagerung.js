191867750d6c02a78abc87f37c39b25411c172e5
matthäus <ma_malek@protonmail.com>
2020-06-23 10:22:41 +0200

    style added

---
+++ src/components/auslagerung.js
@@ -0,0 +1,22 @@
+{
+  /* <h2>New Releases</h2>
+        <div>
+          {releases.map((release) => (
+            <div>
+              <h3
+                onClick={() => {
+                  console.log(release.uri);
+                  setPlayURIs(release.uri);
+                }}
+              >
+                {release.name}
+              </h3>
+              <p>{release.artists[0].name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="">
+        <SpotifyPlayer token={token} uris={playURIs} />
+      </div> */
+}
