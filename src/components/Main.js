03c487f53c0d3a3cb9ce52be3806481fa83191ed
Pensor <patrick.feldtmann@gmail.com>
2020-06-24 15:39:35 +0200

    refactor main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,76 +1,58 @@
-import React, { useEffect, useState } from "react";
-import SpotifyPlayer from "react-spotify-web-playback"; // https://github.com/gilbarbara/react-spotify-web-playback
-import Aside from "./Aside.js";
-import Album from "./Album";
-import LyricsBox from "./lyricsBox/lyricsBox";
-function Main({ token }) {
-  const [releases, setReleases] = useState([]);
-  const [playURIs, setPlayURIs] = useState([]);
-  const [play, setPlay] = useState(null);
-  const [songTitle, setTitle] = useState("");
-  const [songArtist, setArtist] = useState("");
-  useEffect(() => {
-    fetch("https://api.spotify.com/v1/browse/new-releases", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res.albums.items);
-        setReleases(res.albums.items);
-      });
-  }, [token]);
+import React, { useEffect, useState } from 'react';
+import SpotifyPlayer from 'react-spotify-web-playback'; // https://github.com/gilbarbara/react-spotify-web-playback
+import Aside from './Aside.js';
+import Album from './Album';
+import LyricsBox from './lyricsBox/lyricsBox';
 
-  return (
-    <div className="flex flex-col h-screen text-sm text-gray-400">
-      <div className="flex-1 flex overflow-y-hidden">
-        <div className="sidebar w-48 flex-none flex flex-col justify-between bg-gray_aside font-semibold">
-          <div className="py-6">
-            here comes our Logo, import it here "main.js"
-            <Aside token={token} />
-          </div>
-          <div className="py-6"></div>
-          <div className="overflow-y-auto bg-scroll container">
-            <p>Playlists</p>
-          </div>
-        </div>
-        <div className="content-area flex-1 overflow-y-auto bg-gray_content h-screen">
-          This is the content area
-          <h2>New Releases</h2>
-          <div>
-            {releases.map((release) => (
-              <Album
-                setPlay={setPlay}
-                setPlayURIs={setPlayURIs}
-                release={release}
-              />
-            ))}
-          </div>
-        </div>
-        <div className=" w-56 bg-gray-600">
-          {" "}
-          <h2>rechtes frage</h2>
-          <br></br>
-          <h4>PlatzHalter für Osama</h4>
-          <LyricsBox token={token} title={songTitle} artist={songArtist} />
-        </div>
-      </div>
-      <div>
-        <div className="">
-          <SpotifyPlayer
-            token={token}
-            uris={playURIs}
-            play={play}
-            callback={(data) => {
-              setTitle(data.track.name);
-              setArtist(data.track.artists);
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+function Main({ token }) {
+	const [releases, setReleases] = useState([]);
+	const [playURIs, setPlayURIs] = useState([]);
+	const [play, setPlay] = useState(null);
+	const [songTitle, setTitle] = useState('');
+	const [songArtist, setArtist] = useState('');
+
+	useEffect(() => {
+		fetch('https://api.spotify.com/v1/browse/new-releases', {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then(res => res.json())
+			.then(res => {
+				console.log(res.albums.items);
+				setReleases(res.albums.items);
+			});
+	}, [token]);
+
+	return (
+		<div className="text-sm text-gray-400 overflow-y-hidden flex flex-wrap">
+			<Aside token={token} />
+
+			<div className="flex-1 content-area overflow-y-auto bg-gray_content h-screen">
+				This is the content area
+				<h2>New Releases</h2>
+				<div>
+					{releases.map(release => (
+						<Album setPlay={setPlay} setPlayURIs={setPlayURIs} release={release} />
+					))}
+				</div>
+			</div>
+
+			<LyricsBox token={token} title={songTitle} artist={songArtist} />
+
+			<div className="w-full">
+				<SpotifyPlayer
+					token={token}
+					uris={playURIs}
+					play={play}
+					callback={data => {
+						setTitle(data.track.name);
+						setArtist(data.track.artists);
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Main;
--- src/components/lyricsBox/lyricsBox.js
+++ src/components/lyricsBox/lyricsBox.js
@@ -1,45 +1,45 @@
-import React from "react";
-import "./LyricsBox.css";
+import React from 'react';
+import './LyricsBox.css';
 
 class LyricsBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      accessToken: "778e2dfdfda95a7a548f4ca007ad7898",
-      songTitle: this.props.title,
-      artist: this.props.artist,
-      lyrics: "",
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			accessToken: '778e2dfdfda95a7a548f4ca007ad7898',
+			songTitle: this.props.title,
+			artist: this.props.artist,
+			lyrics: '',
+		};
+	}
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.title !== this.props.title) {
-      fetch(
-        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.track.get?q_artist=${this.props.artist}&q_track=${this.props.title}&apikey=${this.state.accessToken}`
-      )
-        .then((res) => res.json())
-        .then((res) => {
-          fetch(
-            `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${res.message.body.track.track_id}&apikey=${this.state.accessToken}`
-          )
-            .then((res) => res.json())
-            .then((res) => {
-              this.setState({ lyrics: res.message.body.lyrics.lyrics_body });
-            });
-        });
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (prevProps.title !== this.props.title) {
+			fetch(
+				`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.track.get?q_artist=${this.props.artist}&q_track=${this.props.title}&apikey=${this.state.accessToken}`
+			)
+				.then(res => res.json())
+				.then(res => {
+					fetch(
+						`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${res.message.body.track.track_id}&apikey=${this.state.accessToken}`
+					)
+						.then(res => res.json())
+						.then(res => {
+							this.setState({ lyrics: res.message.body.lyrics.lyrics_body });
+						});
+				});
+		}
+	}
 
-  render() {
-    return (
-      <div className="lyricsBox">
-        <h1 className="lyricsTitle" onClick={this.UpdateState}>
-          {this.props.title}
-        </h1>
-        <p className="lyrics">{this.state.lyrics}</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="lyricsBox w-56 bg-colorPallete_LightGreen">
+				<h1 className="lyricsTitle" onClick={this.UpdateState}>
+					{this.props.title}
+				</h1>
+				<p className="lyrics">{this.state.lyrics}</p>
+			</div>
+		);
+	}
 }
 
 export default LyricsBox;
