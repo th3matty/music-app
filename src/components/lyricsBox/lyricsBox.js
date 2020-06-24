89850a2c62492dd9e6a26cfbd54c204ed68feb47
osamaAlhaj <osama.alhaj.khaled@gmail.com>
2020-06-24 15:37:50 +0200

    jsx update to show the album name and track name

--- src/components/lyricsBox/lyricsBox.js
+++ src/components/lyricsBox/lyricsBox.js
@@ -6,9 +6,12 @@ class LyricsBox extends React.Component {
     super(props);
     this.state = {
       accessToken: "778e2dfdfda95a7a548f4ca007ad7898",
-      songTitle: this.props.title,
-      artist: this.props.artist,
+      songTitle: this.props.title, // used only to get the lyrics
+      artist: this.props.artist, // used only to get the lyrics
       lyrics: "",
+      album: "",
+      trackName: "",
+      artistName: "",
     };
   }
 
@@ -19,6 +22,11 @@ class LyricsBox extends React.Component {
       )
         .then((res) => res.json())
         .then((res) => {
+          this.setState({
+            artistName: res.message.body.track.artist_name,
+            trackName: res.message.body.track.track_name,
+            album: res.message.body.track.album_name,
+          });
           fetch(
             `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${res.message.body.track.track_id}&apikey=${this.state.accessToken}`
           )
@@ -34,8 +42,10 @@ class LyricsBox extends React.Component {
     return (
       <div>
         <h1 className="lyricsTitle" onClick={this.UpdateState}>
-          {this.props.title}
+          {this.state.trackName}
         </h1>
+        <h3> {this.state.album}</h3>
+        <p>Album from {this.state.artistName} </p>
         <p className="lyrics">{this.state.lyrics}</p>
       </div>
     );
