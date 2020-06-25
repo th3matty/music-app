b7dd17c7d78aaaa9463d4e601dadb6ba5cebdba8
th3matty <ma_malek@protonmail.com>
2020-06-25 13:37:18 +0200

    added UserProfilePicture v.0.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import UserProfileIcon from '../assets/img/userProfile.svg';
+import React, { useState, useEffect } from "react";
+import UserProfileIcon from "../assets/img/userProfile.svg";
 
 function Header({ token }) {
-	const [userProfile, setUserProfile] = useState({});
+  const [userProfile, setUserProfile] = useState({});
 
-	useEffect(() => {
-		fetch('https://api.spotify.com/v1/me', {
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		})
-			.then(res => res.json())
-			.then(profile => setUserProfile(profile));
-	}, []);
+  useEffect(() => {
+    fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((profile) => {
+		console.log("profile", profile);
+		console.log(profile.images[0].url)
+        setUserProfile(profile);
+	  });
+	 
+  }, [token]);
 
-	return (
-		<div className="w-full">
-			<div className="top-bar flex px-4 py-2 justify-end bg-gray_aside">
-				<span>{userProfile.display_name}</span>
-				<a href="!#" className=" mt-2">
-					<img src={UserProfileIcon} className="" alt="UserProfileIcon" />
-				</a>
-			</div>
-		</div>
-	);
+  console.log('userProfile:',userProfile)
+
+//   let userBlankPic;
+
+//   if(userProfile.images ){
+// 	 userBlankPic = userProfile.images[0].url
+//   }else{
+// 	userBlankPic = UserProfileIcon
+//   }
+
+  return (
+    <div className="w-full">
+      <div className="top-bar flex px-4 py-2 justify-end bg-gray_aside">
+        <span>{userProfile.display_name}</span>
+        <a href="!#" className=" mt-2 ml-2">
+          <img src={userProfile.images ? (userProfile.images[0].url) : UserProfileIcon} className="w-10 h-10" alt="UserProfileIcon" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
