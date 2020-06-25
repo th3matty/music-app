f4fc9d5719fac0fd84bf52a182112f467cf479ed
osamaAlhaj <osama.alhaj.khaled@gmail.com>
2020-06-25 16:44:36 +0200

    like button added und can sync with spotify

--- src/components/MiddleFrame.js
+++ src/components/MiddleFrame.js
@@ -1,45 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import TopView from './TopMainView';
-import SpotifyPlayer from 'react-spotify-web-playback'; // https://github.com/gilbarbara/react-spotify-web-playback
+import React, { useState, useEffect } from "react";
+import TopView from "./TopMainView";
+import SpotifyPlayer from "react-spotify-web-playback"; // https://github.com/gilbarbara/react-spotify-web-playback
 
 function MiddleFrame({ token, setTitle, setArtist }) {
-	const [releases, setReleases] = useState([]);
-	const [playURIs, setPlayURIs] = useState([]);
-	const [play, setPlay] = useState(null);
+  const [releases, setReleases] = useState([]);
+  const [playURIs, setPlayURIs] = useState([]);
+  const [play, setPlay] = useState(null);
+  var uris = [];
 
-	useEffect(() => {
-		fetch('https://api.spotify.com/v1/browse/new-releases', {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		})
-			.then(res => res.json())
-			.then(res => {
-				console.log(res.albums.items);
-				setReleases(res.albums.items);
-			});
-	}, [token]);
+  useEffect(() => {
+    fetch("https://api.spotify.com/v1/browse/new-releases", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.albums.items);
+        setReleases(res.albums.items);
+      })
+      .then(
+        fetch("https://api.spotify.com/v1/me/player/recently-played", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          method: "GET",
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            for (let i = 0; i < res.items.length; i++) {
+              uris.push(res.items[i].track.uri);
+              setPlayURIs(uris);
+              console.log("die uris " + playURIs);
+            }
+            //{
+            // console.log("last played " + lastPlayed.track);
+            //
+            // }
+          })
+      );
+  }, [token]);
 
-	return (
-		<div className="flex-1 content-area overflow-y-auto bg-colorPallete_Blue h-screen relative">
-			{/* ContentAREA //<-- TOPVIEW-->//*/}
-			<div>
-				<TopView setPlay={setPlay} setPlayURIs={setPlayURIs} releases={releases} />
-				{/* <TopView setPlay={setPlay} setPlayURIs={setPlayURIs} releases={releases} /> */}
-			</div>
-			<div className="absolute bottom-0 inset-x-0">
-				<SpotifyPlayer
-					token={token}
-					uris={playURIs}
-					play={play}
-					callback={data => {
-						setTitle(data.track.name);
-						setArtist(data.track.artists);
-					}}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex-1 content-area overflow-y-auto bg-colorPallete_Blue h-screen relative">
+      {/* ContentAREA //<-- TOPVIEW-->//*/}
+      <div>
+        <TopView
+          setPlay={setPlay}
+          setPlayURIs={setPlayURIs}
+          releases={releases}
+        />
+        {/* <TopView setPlay={setPlay} setPlayURIs={setPlayURIs} releases={releases} /> */}
+      </div>
+      <div className="absolute bottom-0 inset-x-0">
+        <SpotifyPlayer
+          showSaveIcon={true}
+          token={token}
+          uris={playURIs}
+          play={play}
+          callback={(data) => {
+            setTitle(data.track.name);
+            setArtist(data.track.artists);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default MiddleFrame;
