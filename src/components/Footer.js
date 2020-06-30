c7241321d0b8b6e2c75a8aa1695b72e6a0b9a671
Pensor <patrick.feldtmann@gmail.com>
2020-06-30 15:18:19 +0200

    footer link fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,7 +25,7 @@ function Footer() {
 
   return (
     <div className="px-4 py-2 bg-gray_aside flex items-center">
-      <a href="!#" className="">
+      <a href="" className="">
         <img
           src={aboutUs}
           className="mt-2 ml-2 mb-2"
